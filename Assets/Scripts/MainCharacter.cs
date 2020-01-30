d2853aa25a367df7caac63db4c395a9752cb14f0
itkeman <itamarnine4@gmail.com>
2020-01-30 23:39:20 +0200

    Added navigation package

---
+++ Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 w = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GetComponent<NavMeshAgent2D>().destination = w;
+        }
+    }
+}
