f773ba4f59c5d6cd24010e6e4411a9c258c646d1
Shahari93 <50714305+Shahari93@users.noreply.github.com>
2020-01-31 03:07:24 +0200

    Task UI

---
+++ Assets/Scripts/ChangeColors.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColors : MonoBehaviour
+{
+    [SerializeField] private float _timer;
+    void Update()
+    {
+        ChangeColorToGrey();
+    }
+    void ChangeColorToGrey()
+    {
+        _timer -= 1f*Time.deltaTime;
+        if (_timer <= 0)
+        {
+            _timer = 0;
+            this.gameObject.GetComponent<Image>().color = Color.grey;
+        }
+    }
+}
---
+++ Assets/Scripts/MoveTiles.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MoveTiles : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    [SerializeField]float minY, maxY;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        transform.position = pos;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+            float newY = Input.mousePosition.y;
+            newY -= newY % 150;
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
+}
+
+class OneTask
+{
+    public string Name { get; set; }
+    public int DUration { get; set; }
+}
