93c46eb49025307923df152fea1d7ac3669b1355
Adi Asher <asher10000@gmail.com>
2020-01-31 16:30:39 +0200

    Added Objects, resources folder. updated sink script

--- Assets/Scripts/Sink.cs
+++ Assets/Scripts/Sink.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class Sink : MonoBehaviour
 {
     // Start is called before the first frame update
+    int num_dishes = 0;
     void Start()
     {
         MainObject mainObject = MainObject.Get();
         mainObject.onSomethingHappened += this.listener;
+        StartCoroutine(waiter());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    IEnumerator waiter()
+    {
+        MainObject mainObject = MainObject.Get();
+        yield return new WaitForSeconds(2);
+        mainObject.InvokeEvent(OurEvent.EAT_STOP);
+        yield return new WaitForSeconds(2);
+        mainObject.InvokeEvent(OurEvent.EAT_STOP);
+        yield return new WaitForSeconds(2);
+        mainObject.InvokeEvent(OurEvent.DISHES_STOP);
     }
 
     void listener(OurEvent whatHappened)
@@ -25,7 +37,24 @@ public class Sink : MonoBehaviour
                 Debug.Log("Dishes are starting");
                 break;
 
+            case OurEvent.DISHES_STOP:
+                Debug.Log("Dishes are done");
+                this.num_dishes = 0;
+                break;
+
+            case OurEvent.EAT_STOP:
+                if(this.num_dishes != 2)
+                {
+                    this.num_dishes++;
+                }
+                Debug.Log("More dirt in sink! Num is " + num_dishes.ToString());
+                break;
+            default:
+                return;
         }
+        Sprite sinkSprite = Resources.Load<Sprite>("Sprites/sink" + this.num_dishes.ToString());
+        this.GetComponent<SpriteRenderer>().sprite = sinkSprite;
+        this.GetComponent<SpriteRenderer>().sortingOrder = 1;
 
 
     }
