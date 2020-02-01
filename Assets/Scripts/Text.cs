649edd7ec203fd051136b51f8fdaa425bcbba6ba
Adi Asher <asher10000@gmail.com>
2020-02-01 07:13:19 +0200

    main menu added

---
+++ Assets/Scripts/Text.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Text : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Color color = new Color(0.1f, 0.7f, 0.2f);
+    public bool isStart;
+    public bool isQuit;
+    void Start()
+    {
+        gameObject.GetComponent<TextMeshPro>().color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter()
+    {
+        gameObject.GetComponent<TextMeshPro>().color = color;
+    }
+
+    void OnMouseExit()
+    {
+        gameObject.GetComponent<TextMeshPro>().color = Color.white;
+    }
+
+    void OnMouseUp()
+    {
+        if (isStart)
+        {
+            Application.LoadLevel(1);
+        }
+        if (isQuit)
+        {
+            Application.Quit();
+        }
+    }
+
+}
