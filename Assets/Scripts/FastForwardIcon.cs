a54be3d7158f3bae1063383bc25f34c4c3d56151
itkeman <itamarnine4@gmail.com>
2020-02-01 16:28:45 +0200

    Added fast forward button

---
+++ Assets/Scripts/FastForwardIcon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastForwardIcon : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    public bool Visible
+    {
+        get
+        {
+            return spriteRenderer.color.a == 1f;
+        }
+        set
+        {
+            Color c = spriteRenderer.color;
+            c.a = value ? 1f: 0f;
+            spriteRenderer.color = c;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Visible = Utils.IsFastForward();
+        Debug.Log("ColorA:" + spriteRenderer.color.a.ToString());
+    }
+}
